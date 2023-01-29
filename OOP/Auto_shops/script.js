16bfd31bdbe2c478645c525c2c52b009c2f0e755
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-01-29 15:11:46 +0200

    first commit

---
+++ OOP/Auto_shops/script.js
@@ -0,0 +1,81 @@
+//Sukurkite HTML formą, kurioje vartotojas galės įrašyti (į input laukelius):
+// car brand, model, mileage, price ir image (url laukelis).
+//Per konstruktorių, sukuriams objektas ir jis atvaizduojamas
+//po forma (CSS rašykite CSS'e)
+//Paspaudus ant automobilio nuotraukos- turi alert išmesti kainą.
+
+class CarClass {
+  constructor(brand, model, mileage, price, image) {
+    this.brand = brand;
+    this.model = model;
+    this.mileage = mileage;
+    this.price = price;
+    this.image = image;
+  }
+  carCard() {
+    const containerEl = document.querySelector(".container");
+    const divCardEl = document.createElement("div");
+    const divCardBodyEl = document.createElement("div");
+    const h5CardEl = document.createElement("h5");
+    const h6CardEl1 = document.createElement("h6");
+    const h6CardEl2 = document.createElement("h6");
+    const h6CardEl3 = document.createElement("h6");
+    const imgCardEl = document.createElement("img");
+
+    divCardEl.className = "card";
+    divCardEl.style.width = "18rem";
+    divCardBodyEl.className = "card-body";
+    h5CardEl.className = "card-title";
+    h6CardEl1.className = "card-subtitle mb-2 text-muted";
+    h6CardEl2.className = "card-subtitle mb-2 text-muted";
+    h6CardEl3.className = "card-subtitle mb-2 text-muted";
+    imgCardEl.className = "card-link";
+
+    h5CardEl.innerText = this.brand;
+    h6CardEl1.innerText = `Model: ${this.model}`;
+    h6CardEl2.innerText = `Mileage: ${this.mileage} km`;
+    h6CardEl3.innerText = `Price: ${this.price} Eur`;
+    imgCardEl.innerText = "Picture";
+    imgCardEl.setAttribute("src", this.image);
+    imgCardEl.style.height = "50px";
+    imgCardEl.style.width = "50px";
+
+    containerEl.append(divCardEl);
+    divCardEl.append(divCardBodyEl);
+    divCardBodyEl.append(imgCardEl);
+    divCardBodyEl.append(h5CardEl);
+    divCardBodyEl.append(h6CardEl1);
+    divCardBodyEl.append(h6CardEl2);
+    divCardBodyEl.append(h6CardEl3);
+  }
+}
+
+document
+  .querySelector("#auto-form > button")
+  .addEventListener("click", uploadCar);
+
+function uploadCar(e) {
+  e.preventDefault();
+
+  const carName = document.querySelector("#car-name").value;
+  const carModel = document.querySelector("#car-model").value;
+  const carMileage = document.querySelector("#car-mileage").value;
+  const carPrice = document.querySelector("#car-price").value;
+  const carImage = document.querySelector("#car-image").value;
+
+  const car = new CarClass(carName, carModel, carMileage, carPrice, carImage);
+  // create Card with input parametres
+  car.carCard();
+
+  console.log(car);
+  // Link on foto
+  clickFotoFunc(carName, carModel, carMileage, carPrice, carImage); // paduodam funkcijai parametru, kuriu reikia clickFotoFunc
+}
+
+clickFotoFunc = (carName, carModel, carMileage, carPrice, carImage) => {
+  let cardLinkEl = document.querySelector(".card-link");
+
+  cardLinkEl.addEventListener("click", () => {
+    alert(`${carName} ${carModel} kaina: ${carPrice} Eur.`);
+  });
+};
